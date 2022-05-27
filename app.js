960668139c3debf472fa71004897daca031f1d3e
Atushi Fukuura <82992600+fukuura-tohto@users.noreply.github.com>
2022-05-27 23:37:06 +0900

    1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.0';
-const port = 3000;
+const hostname = 'localhost';//'https://slackapphome.onrender.com'; //'127.0.0.0';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, fukuura!\n');
 });
 
 server.listen(port, hostname, () => {
